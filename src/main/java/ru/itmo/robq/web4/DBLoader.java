b673918b9af039b7f598efea975c8ff77edf6909
Roman Bavykin <bavykin03@outlook.com>
2022-01-24 02:40:34 +0300

    Update DBLoader.java

--- src/main/java/ru/itmo/robq/web4/DBLoader.java
+++ src/main/java/ru/itmo/robq/web4/DBLoader.java
@@ -3,11 +3,13 @@ package ru.itmo.robq.web4;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Example;
 import ru.itmo.robq.web4.data.RoleRepository;
 import ru.itmo.robq.web4.model.Role;
 import ru.itmo.robq.web4.security.userDetails.UserRole;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 
 @Configuration
@@ -16,7 +18,11 @@ public class DBLoader {
     @Bean
     CommandLineRunner runRoles (RoleRepository roleRepo) {
         return args -> {
-            Arrays.stream(UserRole.values()).forEach(role -> roleRepo.save(new Role(role)));
+            Arrays.stream(UserRole.values()).forEach(role -> {
+                if (Objects.isNull(roleRepo.findByRoleName(role))) {
+                    roleRepo.save(new Role(role));
+                }
+            });
         };
     }
 }
